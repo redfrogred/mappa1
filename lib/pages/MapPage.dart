792d0b2c0886570095243bc7a249aa47b7d762df
Matt <matt.garvin.416@gmail.com>
2023-01-29 11:20:26 -0500

    changed button location

--- lib/pages/MapPage.dart
+++ lib/pages/MapPage.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_interpolation_to_compose_strings
 
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import '../classes/Config.dart';
 import '../classes/Utils.dart';
 import 'package:geolocator/geolocator.dart';
@@ -18,6 +19,8 @@ class MapPage extends StatefulWidget {
   State createState() => _MapPageState();
 }
 
+enum MapTypeEnum { normal, terrain, satellite, hybrid }
+
 class _MapPageState extends State<MapPage> {
 
   _MapPageState() {
@@ -26,6 +29,17 @@ class _MapPageState extends State<MapPage> {
 
   // (this page) variables
   static const String _fileName = 'MapPage.dart';
+  //
+  List<String> _colorValue = [ 'black','yellow','green','pink','blue','orange'];
+  List<String> _textValue = [ 'white','black','white','black','white','white'];
+  List<String> _terr = ['none''normal','terrain','satellite','hybrid'];
+  List<String> _boolColorValue = [ 'white','black''white','black'];
+
+  bool _trafficFlag = false;
+  int _trafficInt = 1;
+    int _terrIndex = 1;
+  MapType _currentMapType = MapType.normal;
+
   // marker locations
   static const double y = 45.3166228;   // initial camera y position
   static const double x = -79.2299500;  // initial camera x position
@@ -37,11 +51,12 @@ class _MapPageState extends State<MapPage> {
   final Map<String, Marker> _markers = {};
   final CameraPosition _defaultCamera = CameraPosition(
     target: LatLng(y,x),
-    zoom: 13,
+    zoom: 15,
   );
 
-  static const String mapStyleJSON = "[{\"featureType\":\"administrative.land_parcel\",\"elementType\":\"labels\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi\",\"elementType\":\"labels.text\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi.business\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road\",\"elementType\":\"labels.icon\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road.local\",\"elementType\":\"labels\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"transit\",\"stylers\":[{\"visibility\":\"off\"}]}]";
-
+  //static const String mapStyleJSON = "[{\"featureType\":\"administrative.land_parcel\",\"elementType\":\"labels\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi\",\"elementType\":\"labels.text\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi.business\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road\",\"elementType\":\"labels.icon\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road.local\",\"elementType\":\"labels\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"transit\",\"stylers\":[{\"visibility\":\"off\"}]}]";
+  //static const String mapStyleJSON = "[{\"featureType\":\"administrative.land_parcel\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"administrative.neighborhood\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi\",\"elementType\":\"labels.text\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi.business\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road\",\"elementType\":\"labels\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road\",\"elementType\":\"labels.icon\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"transit\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"water\",\"elementType\":\"labels.text\",\"stylers\":[{\"visibility\":\"off\"}]}]";
+  static const String mapStyleJSON = "[{\"featureType\":\"administrative\",\"elementType\":\"geometry\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"poi\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"road\",\"elementType\":\"labels.icon\",\"stylers\":[{\"visibility\":\"off\"}]},{\"featureType\":\"transit\",\"stylers\":[{\"visibility\":\"off\"}]}]";
   // (this page) init and dispose
   @override
   void initState() {
@@ -61,6 +76,25 @@ class _MapPageState extends State<MapPage> {
     Utils.log('( $_fileName ) _buildTriggered()');
   }
 
+  void _tapMapType() {
+    if (_terrIndex == 4) {
+      _terrIndex = 0;
+    }
+
+    setState(() {
+      _terrIndex++;
+      _currentMapType = MapType.values[_terrIndex];
+    });
+  }
+
+  void _tapTraffic() {
+   // _trafficFlag ? !_trafficFlag : _trafficFlag;
+
+    setState(() {
+      _trafficFlag == true ? _trafficFlag = false : _trafficFlag = true;
+      _trafficFlag == true ? _trafficInt = 0 : _trafficInt = 1;
+    });
+  }  
   
   void _addPostFrameCallbackTriggered( context ) {
     Utils.log('( $_fileName ) _addPostFrameCallbackTriggered()');
@@ -148,29 +182,42 @@ class _MapPageState extends State<MapPage> {
                 },
                 markers: _markers.values.toSet(),
                 mapToolbarEnabled: false,
-                //mapType: MapType.terrain,
+                mapType: _currentMapType, 
                 myLocationEnabled: true,
                 myLocationButtonEnabled: true,
-                //trafficEnabled: true,
+                trafficEnabled: _trafficFlag,
               ),
               Positioned(
                 child: Container(child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('( hud )'),
-                ))),
-              Positioned(
-                left: 0,
-                bottom: 10,
-                child: Container(
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  child: Center(
-                    child: ElevatedButton(
-                      child: Text('START'),
-                      onPressed: () {},
+                  padding: const EdgeInsets.all(1.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children : [ 
+                        ElevatedButton(
+                          child: Text('START'),
+                          onPressed: () {},
+                        ),                        
+                        ElevatedButton(
+                          child: Text( MapType.values[_terrIndex].toString() ),
+                          onPressed: () {
+                            _tapMapType();
+                          },
+                        ),                         
+                        ElevatedButton(
+                          child: Text( 'Traffic.' + _trafficFlag.toString() ),
+                          onPressed: () {
+                            _tapTraffic();
+                          },
+                        ),                       
+
+                    ]
                     ),
-                  )
-                )),                
+                  ),
+                ))),
+               
             ],
           ),
         ),
